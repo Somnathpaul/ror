3c57d7102b54b86d614bad95410a33ac9f916199
Somnath Paul <Somnath4x@gmail.com>
2020-03-02 19:15:18 +0530

    Add routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+    # home page method
+    def home
+        
+    end
+
+    # about page method
+    def about
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # home roure
+  get 'pages/home', to: 'pages#home'
+  # about page
+  get 'pages/about', to: 'pages#about'
 end
